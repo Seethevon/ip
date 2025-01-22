41a528b745307b1dee50592769a8a316d05b5c7e
Seethevon <seevon.tan@gmail.com>
2025-01-22 20:41:42 +0800

    Revert "test"
    This reverts commit 7834638334f95928418c2794e3cc9b08c3b99958.

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -6,6 +6,5 @@ public class Bibi {
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
-        System.out.println("____________________________________________________________");
     }
 }
