2027ac051a1b403371d4ab50794dc633f620ca8d
Seethevon <seevon.tan@gmail.com>
2025-01-22 20:54:07 +0800

    level-1

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
 public class Bibi {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Bibi");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                // Exit condition
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
 
