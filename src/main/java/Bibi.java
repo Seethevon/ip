6d685ef76bce2bc3d4ccfc010156ea9fd106a274
Seethevon <seevon.tan@gmail.com>
2025-01-22 21:07:41 +0800

    level-3

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Bibi {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -6,8 +7,7 @@ public class Bibi {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        String[] tasks = new String[100];
-        int taskC = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String input;
@@ -22,13 +22,27 @@ public class Bibi {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < taskC; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                tasks.get(taskNumber).markDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks.get(taskNumber));
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                tasks.get(taskNumber).markNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks.get(taskNumber));
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[taskC] = input;
-                taskC++;
+                // Add a new task
+                tasks.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
