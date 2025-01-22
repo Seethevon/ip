cecba8b143ec94fe8ffbbcc794967cf534c1e85c
Seethevon <seevon.tan@gmail.com>
2025-01-22 20:58:52 +0800

    level-2

--- src/main/java/Bibi.java
+++ src/main/java/Bibi.java
@@ -6,21 +6,33 @@ public class Bibi {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] tasks = new String[100];
+        int taskC = 0;
+
         Scanner scanner = new Scanner(System.in);
         String input;
 
         while (true) {
             input = scanner.nextLine();
+
             if (input.equals("bye")) {
-                // Exit condition
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskC; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskC] = input;
+                taskC++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
