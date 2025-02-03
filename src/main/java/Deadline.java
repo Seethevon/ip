b5899f5383e527bbfe142e108ee1983b71e956cb
Seethevon <seevon.tan@gmail.com>
2025-02-04 01:06:40 +0800

    implementing date & time level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Deadline extends Task {
-    protected String by;
+    private LocalDateTime by;
+
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+    }
+
+    public LocalDateTime getBy() {
+        return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm")) + ")";
     }
 }
